325462beeee4833fc786edcf86eb98d932616e5e
Samiha Jebril <samihajebril10@gmail.com>
2020-12-02 09:59:02 +0100

    feat (header)

---
+++ header.js
@@ -0,0 +1,20 @@
+export const header = `
+<nav>
+  <ul>
+  <li><img src="logo.png" class ="image"></li>
+  <li><a href="#">Home</a></li>
+  <li><a href="#">Destination</a></li>
+  <li><a href="#">About us</a></li>
+  
+    <li><a href="#">Contact us</a></li>
+    
+    <li class="deroulant"><a href="#">EN</a>
+      <ul class="sous">
+        <li><a href="#">FR</a></li>
+        
+      </ul>
+    </li>
+  </ul>
+</nav>
+
+`;
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 import './style.scss';
-// import $ from 'jquery';
+ import $ from 'jquery';
 // import axios from 'axios';
+import {header} from "./header"
+document.querySelector('main').innerHTML = header;

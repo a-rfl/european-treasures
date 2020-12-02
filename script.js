a033fa1dc14c0aa11f8c0914ec7a0aafca06f343
Samiha Jebril <samihajebril10@gmail.com>
2020-12-02 22:23:34 +0100

    added section-head

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ import './style.scss';
 import $ from 'jquery';
 // import axios from 'axios';
 import { header } from './header';
-import { header2 } from './header2';
 
-document.querySelector('header').innerHTML = header2;
+document.querySelector('header').innerHTML = header;

0bd5b6986eea5018b8dd9fd85da4cc711d881e15
a-rfl <59601726+a-rfl@users.noreply.github.com>
2020-12-19 11:08:19 +0100

    Add comments

--- src/services/section-destinations.js
+++ src/services/section-destinations.js
@@ -1,5 +1,9 @@
+// (Array) => String
 export const sectionDestinations = (arr) => {
   const categoriesList = [];
+  // ForEach element of the array, it will check is proprety 'category'
+  // And will put the value of each in a new array (categoriesList)
+  // but only if the value is not already in the new array
   arr.forEach((place) => {
     place.categories.forEach((cat) => {
       if (!categoriesList.includes(cat)) {
@@ -16,6 +20,9 @@ export const sectionDestinations = (arr) => {
                 <select name="filter" id="filter">
                     <option value="all" selected>tout</option>
     `;
+    // Creation of the options' select
+    // By creating an option by element of the categoryList
+    // Each option has the attribute value and the inner html of an element of categoriesList
   categoriesList.forEach((category) => {
     destinations += `<option value="${category}">${category}</option>`;
   });
@@ -32,6 +39,8 @@ export const sectionDestinations = (arr) => {
             </div>
             <section class="destinations">
     `;
+  // Loop on the array, to get the first image, the index and the name of each ellement
+  // in the array passed in argument
   arr.forEach((place, index) => {
     destinations += `
             <div class="destination" style="background-image: url(images/${place.images[0]});" id="${index}">

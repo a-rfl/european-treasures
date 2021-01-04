d0327ec96a1e82d4fffd928114342d3aadf67973
a-rfl <59601726+a-rfl@users.noreply.github.com>
2021-01-04 18:01:15 +0100

    Fix issue : click on header not accessible

--- src/services/section-destinations.js
+++ src/services/section-destinations.js
@@ -1,32 +1,33 @@
 // (Array) => String
 export const headerDestinations = () => {
   const destinations = `
-        <div class="destinations-header">
-            <h2 class="destinations-title" >Destinations</h2>
-            <div class="filters">
-                <select name="filter" id="filter">
-                    <option value="all" selected>tout</option>
-                    <option value="cascade">cascades</option>
-                    <option value="forêt">forêts</option>
-                    <option value="grotte">grottes</option>
-                    <option value="lac">lacs</option>
-                    <option value="volcan">volcans</option>
-                </select>
+    <h2 class="destinations-title" >Destinations</h2>
+    <div class="destinations-header">
+    
+      <div class="filters">
+        <select name="filter" id="filter">
+            <option value="all" selected>tout</option>
+            <option value="cascade">cascades</option>
+            <option value="forêt">forêts</option>
+            <option value="grotte">grottes</option>
+            <option value="lac">lacs</option>
+            <option value="volcan">volcans</option>
+        </select>
 
-                <button class="random btn-filter">Lieu aléatoire</button>
+        <button class="random btn-filter">Lieu aléatoire</button>
 
-                <button class="map btn-filter">Carte</button>
-            </div>
-        </div>`;
+        <button class="map btn-filter">Carte</button>
+      </div>
+    </div>`;
 
   return destinations;
 };
 
 export const contentDestinations = (arr) => {
   let destinations = '<section class="destinations">';
-  // Loop on the array, to get the first image, the index and the name of each ellement
+  // Loop on the array, to get the first image, the id and the name of each ellement
   // in the array passed in argument
-  arr.forEach((place, index) => {
+  arr.forEach((place) => {
     destinations += `
             <div class="destination" style="background-image: url(images/${place.images[0]});" id="${place.id - 1}">
                 <h3>${place.nom}</h3>
